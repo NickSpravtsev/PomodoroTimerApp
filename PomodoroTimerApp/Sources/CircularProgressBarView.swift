0305df10637a45478ef15db00c3031cc39b7c886
Nikolay Spravtsev <nicksprv@icloud.com>
2022-08-25 00:16:14 +0300

    Progress bar added. Timer accuracy improved

--- PomodoroTimerApp/Sources/CircularProgressBarView.swift
+++ PomodoroTimerApp/Sources/CircularProgressBarView.swift
@@ -9,11 +9,82 @@ import UIKit
 
 class CircularProgressBarView: UIView {
 
+    // MARK: - Properties
+
+    private var progressLayer = CAShapeLayer()
+    private var circleLayer = CAShapeLayer()
+    private var startPoint = CGFloat()
+    private var endPoint = CGFloat()
+
+    // MARK: - Inits
+
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
 
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+    }
+
+    // MARK: - Methods
+
+    func createCircularPath(color: UIColor, clockwise: Bool) {
+
+        if clockwise {
+            startPoint = CGFloat(-Double.pi / 2)
+            endPoint = CGFloat(3 * Double.pi / 2)
+        } else {
+            startPoint = CGFloat(3 * Double.pi / 2)
+            endPoint = CGFloat(-Double.pi / 2)
+        }
+
+        let circularPath = UIBezierPath(
+                                        arcCenter: CGPoint(x: frame.size.width / 2.0, y: frame.size.height / 2.0),
+                                        radius: 150,
+                                        startAngle: startPoint,
+                                        endAngle: endPoint, clockwise: clockwise)
+
+        circleLayer.path = circularPath.cgPath
+        circleLayer.fillColor = UIColor.clear.cgColor
+        circleLayer.lineCap = .round
+        circleLayer.lineWidth = 3.0
+        circleLayer.strokeEnd = 1
+        circleLayer.strokeColor = UIColor.white.cgColor
+        layer.addSublayer(circleLayer)
+
+        progressLayer.path = circularPath.cgPath
+        progressLayer.fillColor = UIColor.clear.cgColor
+        progressLayer.lineCap = .round
+        progressLayer.lineWidth = 10.0
+        progressLayer.strokeEnd = 0
+        progressLayer.strokeColor = color.cgColor
+        layer.addSublayer(progressLayer)
+    }
+
+    func progressAnimation(duration: TimeInterval, autostart: Bool) {
+        let circulatProgressAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        circulatProgressAnimation.duration = duration
+        circulatProgressAnimation.toValue = 1.0
+        circulatProgressAnimation.fillMode = .forwards
+        circulatProgressAnimation.isRemovedOnCompletion = false
+        progressLayer.add(circulatProgressAnimation, forKey: "progressAnim")
+        if !autostart {
+            pauseAnimation()
+        }
+    }
+
+    func pauseAnimation() {
+        let pausedTime = layer.convertTime(CACurrentMediaTime(), from: nil)
+        layer.speed = 0.0
+        layer.timeOffset = pausedTime
+    }
+
+    func resumeAnimation() {
+        let pausedTime = layer.timeOffset
+        layer.speed = 1.0
+        layer.timeOffset = 0.0
+        layer.beginTime = 0.0
+        let timeSincePause = layer.convertTime(CACurrentMediaTime(), from: nil) - pausedTime
+        layer.beginTime = timeSincePause
     }
 }
--- PomodoroTimerApp/Sources/ViewController.swift
+++ PomodoroTimerApp/Sources/ViewController.swift
@@ -16,25 +16,40 @@ class ViewController: UIViewController {
     private var isWorkTime = true
     private var isTimerStarted = false
 
-    //Timer
+    // Timer
     private var timer: Timer?
     private var workSeconds = 25
     private var restSeconds = 5
     private var currentSecondsRemain = 25
+    private var accurateTimerCount = 1000
+
+    // Progress bar
+    private lazy var circularProgressBarView = CircularProgressBarView(frame: .zero)
+    private var circularViewDuration = 5
 
     // MARK: - Methods
 
     private func runTimer() {
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: (#selector(updateViewByTimer)), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: (#selector(updateViewByTimer)), userInfo: nil, repeats: true)
     }
 
     @objc private func updateViewByTimer() {
-        if currentSecondsRemain < 1 {
+
+        // To improve accuration of timer
+        if accurateTimerCount > 0 {
+            accurateTimerCount -= 1
+            return
+        }
+
+        accurateTimerCount = 1000
+
+        if currentSecondsRemain < 0 {
             timer?.invalidate()
         } else {
             currentSecondsRemain -= 1
             timerLabel.text = timeInSecondsToString(seconds: currentSecondsRemain)
-            if currentSecondsRemain == 0 {
+            if currentSecondsRemain == -1 {
+                timerLabel.text = timeInSecondsToString(seconds: 0)
                 timer?.invalidate()
                 changeTimePeriod()
             }
@@ -47,14 +62,18 @@ class ViewController: UIViewController {
             controlButton.tintColor = .systemGreen
             timerLabel.textColor = .systemGreen
             timerLabel.text = timeInSecondsToString(seconds: restSeconds)
+            setupCircularProgressBarView(color: .systemGreen, duration: restSeconds, autostart: true, clockwise: false)
             currentSecondsRemain = restSeconds
+            accurateTimerCount = 1000
             runTimer()
         } else {
             isWorkTime = true
             controlButton.tintColor = .systemRed
             timerLabel.textColor = .systemRed
             timerLabel.text = timeInSecondsToString(seconds: workSeconds)
+            setupCircularProgressBarView(color: .systemRed, duration: workSeconds, autostart: true, clockwise: true)
             currentSecondsRemain = workSeconds
+            accurateTimerCount = 1000
             runTimer()
         }
     }
@@ -71,8 +90,8 @@ class ViewController: UIViewController {
 
     private lazy var timerLabel: UILabel = {
         let label = UILabel()
-        label.text = timeInSecondsToString(seconds: workSeconds)
-        label.font = UIFont.systemFont(ofSize: 50)
+        label.text = timeInSecondsToString(seconds: currentSecondsRemain)
+        label.font = UIFont.systemFont(ofSize: 70)
         label.textColor = .systemRed
 
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -81,10 +100,20 @@ class ViewController: UIViewController {
 
     private lazy var controlButton: UIButton = {
         let button = UIButton()
-        button.setImage(UIImage(systemName: "play"), for: .normal)
+        button.setImage(UIImage(systemName: "play.fill"), for: .normal)
         button.contentHorizontalAlignment = .fill
         button.contentVerticalAlignment = .fill
         button.tintColor = .systemRed
+
+        button.layer.cornerRadius = 20
+
+        // Making shadow for button
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.5
+        button.layer.shadowOffset = .zero
+        button.layer.shadowRadius = 10
+        button.layer.shouldRasterize = true
+        button.layer.rasterizationScale = UIScreen.main.scale
 
         button.addTarget(self, action: #selector(controlButtonPressed), for: .touchUpInside)
 
@@ -111,6 +140,7 @@ class ViewController: UIViewController {
     private func setupLayout() {
         setupTimerLabel()
         setupControlButton()
+        setupCircularProgressBarView(color: UIColor.systemRed, duration: currentSecondsRemain, autostart: false, clockwise: true)
     }
 
     private func setupTimerLabel() {
@@ -122,11 +152,18 @@ class ViewController: UIViewController {
 
     private func setupControlButton() {
         controlButton.snp.makeConstraints { make in
-            make.top.equalTo(timerLabel.snp.bottom).offset(50)
+            make.top.equalTo(timerLabel.snp.bottom).offset(170)
             make.centerX.equalTo(view)
-            make.width.equalTo(50)
-            make.height.equalTo(50)
+            make.width.equalTo(70)
+            make.height.equalTo(70)
         }
+    }
+
+    private func setupCircularProgressBarView(color: UIColor, duration: Int, autostart: Bool, clockwise: Bool) {
+        circularProgressBarView.createCircularPath(color: color, clockwise: clockwise)
+        circularProgressBarView.center = view.center
+        circularProgressBarView.progressAnimation(duration: TimeInterval(duration), autostart: autostart)
+        view.addSubview(circularProgressBarView)
     }
 
     // MARK: - Actions
@@ -135,11 +172,17 @@ class ViewController: UIViewController {
 
         if isTimerStarted {
             timer?.invalidate()
-            controlButton.setImage(UIImage(systemName: "play"), for: .normal)
+            controlButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
+
+            circularProgressBarView.pauseAnimation()
+            
             isTimerStarted = false
         } else {
             runTimer()
-            controlButton.setImage(UIImage(systemName: "pause"), for: .normal)
+            controlButton.setImage(UIImage(systemName: "pause.fill"), for: .normal)
+
+            circularProgressBarView.resumeAnimation()
+
             isTimerStarted = true
         }
     }
